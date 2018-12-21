b8e7b12304c4d541f9fac6800cb528e397e39de2
TNtLike <40592693+TNtLike@users.noreply.github.com>
2018-12-21 16:21:43 +0800

    3

---
+++ polls/testdb.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+
+from .models import polls_model
+
+# 数据库操作
+
+
+def insert(request):
+    test1 = polls_model(name='Alex-Qiu')
+    test1.save()
+    return HttpResponse('<p>数据添加成功！</p>')
+
+
+def show(request):
+    response = '',
+    list = polls_model.objects.filter(name="Alex-Qiu").order_by("id")
+    for value in list:
+        response = value.name
+
+    return HttpResponse('<p>'+response+'<p>')
--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,12 @@
 
 from django.urls import include, path
 from . import views
+from . import testdb
 
 urlpatterns = [
 
     path('polls/', views.index, name='index'),
     path('polls/hello/', views.polls, name='hello'),
+    path('polls/testdb/', testdb.insert),
+    path('polls/show/',testdb.show)
 ]
